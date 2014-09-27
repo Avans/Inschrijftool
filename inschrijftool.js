501fd90cd0cacc3998eda9e6a8d559a54505c15d
Paul Wagener <mail@paulwagener.nl>
2014-09-27 16:41:15 +0200

    Avans login works

--- inschrijftool.js
+++ inschrijftool.js
@@ -1,23 +1,13 @@
+Courses = new Mongo.Collection("courses");
+Enrollments = new Mongo.Collection("enrollments");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
+  Template.enroll.helpers({
+    course: function () {
+      return Courses.find({'url': window.location.pathname});
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
-    }
-  });
-}
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
 }
